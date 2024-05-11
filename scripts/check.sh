057b012af18eba9a46a367900c63d1f73e1e1cbd
Vladimir Jovanovic <vladimirjo@gmail.com>
2024-05-12 00:50:02 +0200

    fix code check in github actions

--- scripts/check.sh
+++ scripts/check.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
-# Function to check if all commands return 0
+# Define a success message
+success() {
+    local prefix="\e[1;32m========================================\n\e[1;32m"
+    local sufix="\n\e[1;32m========================================\e[0m"
+    local result="${prefix}$1${sufix}"
+    echo "$result"
+}
 
+# Define a warning message
+warning() {
+    local prefix="\e[1;31m========================================\n\e[1;31m"
+    local sufix="\n\e[1;31m========================================\e[0m"
+    local result="${prefix}$1${sufix}"
+    echo "$result"
+}
 
 # Function to check if .venv directory exists
 check_venv() {
@@ -24,26 +37,41 @@ check_result=0
 ruff check
 if [ $? -ne 0 ]; then
     check_result=1
+    echo -e "$(warning "Linting Failed")"
+else
+    echo -e "$(success "Linting Successful")"
 fi
 
 ruff format --check
 if [ $? -ne 0 ]; then
     check_result=1
+    echo -e "$(warning "Formatting Failed")"
+else
+    echo -e "$(success "Formatting Successful")"
 fi
 
 mypy src/
 if [ $? -ne 0 ]; then
     check_result=1
+    echo -e "$(warning "Check Type Failed")"
+else
+    echo -e "$(success "Check Type Successful")"
 fi
 
 pytest --cov=docugenr8 tests/
 if [ $? -ne 0 ]; then
     check_result=1
+    echo -e "$(warning "Tests Failed")"
+else
+    echo -e "$(success "Tests Successful")"
 fi
 
 source ./scripts/version.sh
 if [ $? -ne 0 ]; then
     check_result=1
+    echo -e "$(warning "Version exists on PyPI")"
+else
+    echo -e "$(success "Version does not exists on PyPI")"
 fi
 
 # Deactivate virtual environment
